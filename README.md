a3bff152d0478d60c6171647323d905f711a13ec
Stephan Baller <stephanballer@gmx.de>
2021-01-20 18:59:40 +0100

    bug fixes

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # pyhm310p
+
+Python library to use with the Hanmatek HP310P
+
+Dependencies:
+ - pymodbus
+
+Files:
+ - hm310p.py: Actual library
+ - serial_reader.py: Simple command line interface for monitoring with the option to save data to file and plot afterwards
+
+Example:
+```
+from hm310p import HMReader
+from time import sleep
+
+reader = HMReader('/dev/ttyUSB0')
+while True:
+    print(reader.readVoltage())
+    sleep(1)
+```
--- hm310p.py
+++ hm310p.py
@@ -9,16 +9,16 @@ regs = {'powerSwitch':0x01,
         'protectVoltage':0x20,
         'protectCurrent':0x21,
         'protectPower':0x22,
-        'setVoltage':0x30,
-        'setCurrent':0x31,
-        'setTimeSpan':0x32,
         'decimals':0x05,
-        'm1':0x00,
-        'm2':0x10,
-        'm3':0x20,
-        'm4':0x30,
-        'm5':0x40,
-        'm6':0x50,
+        'ps':0x0010,
+        'set':0x0030,
+        'psm':0x1000,
+        'm1':0x1000,
+        'm2':0x1010,
+        'm3':0x1020,
+        'm4':0x1030,
+        'm5':0x1040,
+        'm6':0x1050,
         'Voltage':0x00,
         'Current':0x01,
         'Time':0x02,
@@ -32,9 +32,10 @@ class HMReader:
  
 
     def readModel(self):
-        return self._client.read_holding_registers(regs[model])
+        return self._client.read_holding_registers(regs['model'],
+                1, unit=UNIT).registers[0]
 
-    def read(self, register='m2'):
+    def read(self, register='ps'):
         ret = dict(zip(['Voltage', 'Current', 'Time', 'Power'],
             self._client.read_holding_registers(regs[register],
             4, unit=UNIT).registers))
@@ -43,19 +44,19 @@ class HMReader:
         ret['Power'] = ret['Power'] / 1000.0
         return ret
     
-    def readVoltage(self, register='m2'):
+    def readVoltage(self, register='ps'):
         return self._client.read_holding_registers(regs[register] +
                 regs['Voltage'], 1, unit=UNIT).registers[0]/100.0
     
-    def readCurrent(self, register='m2'):
+    def readCurrent(self, register='ps'):
         return self._client.read_holding_registers(regs[register] +
                 regs['Current'], 1, unit=UNIT).registers[0]/1000.0
     
-    def readTime(self, register='m2'):
+    def readTime(self, register='ps'):
         return self._client.read_holding_registers(regs[register] +
                 regs['Time'], 1, unit=UNIT).registers[0]
     
-    def readPower(self, register='m2'):
+    def readPower(self, register='ps'):
         return self._client.read_holding_registers(regs[register] +
                 regs['Power'], 1, unit=UNIT).registers[0]
 
@@ -63,16 +64,19 @@ class HMReader:
         self._client.write_register(regs['powerSwitch'], 1 if val else 0, unit=UNIT)
 
     def setProtectVoltage(self, val):
-        self._client.write_register(regs['protectVoltage'], val*100.0, unit=UNIT)
+        self._client.write_register(regs['protectVoltage'], int(val*100.0), unit=UNIT)
 
-    def setProtectVoltage(self, val):
-        self._client.write_register(regs['protectCurrent'], val*1000.0, unit=UNIT)
+    def setProtectCurrent(self, val):
+        self._client.write_register(regs['protectCurrent'], int(val*1000.0), unit=UNIT)
 
-    def setVoltage(self, val):
-        self._client.write_register(regs['voltage'], val*100.0, unit=UNIT)
+    def setVoltage(self, val, register='set'):
+        self._client.write_register(regs[register] + regs['Voltage'], int(val*100), unit=UNIT)
 
-    def setCurrent(self, val):
-        self._client.write_register(regs['current'], val*1000.0, unit=UNIT)
+    def setCurrent(self, val, register='set'):
+        self._client.write_register(regs[register] + regs['Current'], int(val*1000), unit=UNIT)
+
+    def setTimeSpan(self, val, register='set'):
+        self._client.write_register(regs[register] + regs['Time'], int(val*1000), unit=UNIT)
 
     def close(self):
         self._client.close()
